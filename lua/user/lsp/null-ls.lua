d4b27f3c459386a6450798d0cac56a02881b8e85
kerron <kerron68@gmail.com>
2022-03-23 09:34:52 +0000

    feat: prettier and formatting changes

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -7,13 +7,26 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local actions = null_ls.builtins.code_actions
+
+local on_attach = function(client)
+	if client.resolved_capabilities.document_formatting then
+		vim.cmd([[
+          augroup LspFormatting
+              autocmd! * <buffer>
+              autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+          augroup END
+          ]])
+	end
+end
 
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.prettierd,
 		formatting.stylua,
-    -- diagnostics.flake8
+		actions.eslint,
+		-- diagnostics.flake8
 	},
+	on_attach = on_attach,
 })
